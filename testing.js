43520aa85c85cf1f11f1349dbdd0cdd9534b5f74
John Lee <johnmlee101@gmail.com>
2016-09-20 22:18:28 -0700

    Added callback support for get commands

--- testing.js
+++ testing.js
@@ -1,4 +1,5 @@
 var GHProjectManager = require('./index.js');
+const _ = require('underscore');
 
 const repoOwner = 'johnmlee101';
 const repo = 'GitHub-Project-Manager';
@@ -6,23 +7,31 @@ const repo = 'GitHub-Project-Manager';
 // User PW
 var manager = new GHProjectManager('johnmlee101-test', process.env.MY_SECRET_ENV);
 
+function callback(r, code) {
+	console.log(r);
+	console.log(code);
+};
+
+function errorCallback(e, code) {
+	if (!_.isUndefined(e)) console.log(`[ERROR] Server returned ${e}`);
+	console.log("[ERROR] Return code returned: "+code);
+	process.exit(1);
+};
+
 if (manager.credentials.username === 'user') {
 	console.log("Need to provide your own credentials!!!\n");
 	process.exit(1);
 }
 
 // Cache Testing
-manager.GetColumns(repoOwner,repo,1, true);
-manager.GetProjects(repoOwner,repo, true);
+manager.GetColumns(repoOwner, repo, 1, callback, errorCallback, true);
+manager.GetProjects(repoOwner, repo, callback, errorCallback, true);
 setTimeout(() => {
-	manager.GetColumns(repoOwner,repo,1, true);
+	manager.GetColumns(repoOwner, repo, 1, callback, errorCallback, true);
 	setTimeout(() => {
-		manager.GetProjects(repoOwner,repo, true);
-		setTimeout(() => {
-			console.log(manager.cache);
-		},2000);	
+		manager.GetProjects(repoOwner, repo, callback, errorCallback, true);
 	},2000);
 },2000);
 
 // Card Testing
-//manager.GetCards('johnmlee101','GitHub-Project-Manager', 106445, true);
+manager.GetCards('johnmlee101', 'GitHub-Project-Manager', 106445, true);
