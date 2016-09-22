2c632bcac080f11c92770b61cf92508e8fe903d4
John Lee <johnmlee101@gmail.com>
2016-09-21 19:04:15 -0700

    Uses promises instead of async calls (#9)

--- testing.js
+++ testing.js
@@ -18,51 +18,130 @@ function errorCallback(e, code) {
 	process.exit(1);
 };
 
-function AssertCodeCallback(code, context = "") {
-	return (e, responseCode) => {
-		if (responseCode != code ) {
-			console.log(`[ASSERT] ${context} Assert failed!`);
-			process.exit(1);
-		} else {
-			console.log(`[ASSERT] ${context} Check. ${responseCode} = ${code}`);
-		}
-	};
+function AssertCodeCallback(data, code, context = "") {
+	context += " [Code]";
+	if (data.statusCode != code ) {
+		console.log(`[ASSERT] ${context} Assert failed! ${data.statusCode} != ${code}`);
+		process.exit(1);
+	} else {
+		console.log(`[ASSERT] ${context} Check. ${data.statusCode} = ${code}`);
+	}
+}
+
+function AssertCountCallback(data, count, context = "") {
+	context += " [Count]";
+	if ( data.response.length != count ) {
+		console.log(`[ASSERT] ${context} Assert failed! Wanted ${data.response.length} size but got ${count}`);
+		process.exit(1);
+	} else {
+		console.log(`[ASSERT] ${context} Check. ${data.response.length} = ${count}`);
+	}
 }
 
 // Cache Testing
-function testCache() {
+function testCache(resolve, reject) {
 	const context = "[testCache]";
-	manager.GetColumns(repoOwner, repo, 1, AssertCodeCallback(200, context), errorCallback, false);
-	manager.GetProjects(repoOwner, repo, AssertCodeCallback(200, context), errorCallback, false);
-	setTimeout(() => {
-		manager.GetColumns(repoOwner, repo, 1, AssertCodeCallback(304, context), errorCallback, false);
-		setTimeout(() => {
-			manager.GetProjects(repoOwner, repo, AssertCodeCallback(304, context), errorCallback, false);
-		},2000);
-	},2000);
+	manager.ClearCache();
+	manager.GetColumns(repoOwner, repo, 1, false)
+	.then(
+		(data) => {
+			AssertCodeCallback(data, 200, context);
+			manager.GetColumns(repoOwner, repo, 1, false)
+			.then(
+				(data) => {
+					AssertCodeCallback(data, 304, context);
+					resolve();
+				});
+		});
 }
 
 // Card Testing
-function testCards() {
+function testCards(resolve, reject) {
 	const context = "[testCards]";
-	manager.GetCards('johnmlee101', 'GitHub-Project-Manager', 106445, AssertCodeCallback(200, context), errorCallback, false);
+	manager.ClearCache();
+	manager.GetCards(repoOwner, 'GitHub-Project-Manager', 106445, false)
+	.then(
+		(data) => {
+			AssertCodeCallback(data, 200, context);
+			resolve();
+		});
+}
+
+function testProjects(resolve, reject) {
+	const context = "[testProjects]";
+	manager.ClearCache();
+	manager.GetProjects(repoOwner, repo, false)
+	.then(
+		(data) => {
+			AssertCodeCallback(data, 200, context);
+			resolve();
+		});
+}
+
+function testColumns(resolve, reject) {
+	const context = "[testColumns]";
+	manager.ClearCache();
+	manager.GetColumns(repoOwner, repo, 1, false)
+	.then(
+		(data) => {
+			AssertCodeCallback(data, 200, context);
+			resolve();
+		});
+}
+
+function testIssue(resolve, reject) {
+	const context = "[testIssue]";
+	manager.ClearCache();
+	manager.GetIssue(repoOwner, repo, 178260045, false)
+	.then(
+		(data) => {
+			AssertCodeCallback(data, 200, context);
+			resolve();
+		});
 }
 
 // Issue Testing
-function testIssues() {
+function testIssues(resolve, reject) {
+	manager.ClearCache();
 	const context = "[testIssues]";
-	manager.GetIssues('johnmlee101', 'GitHub-Project-Manager', AssertCodeCallback(200, context), errorCallback, false);
+	manager.GetIssues(repoOwner, repo, false)
+	.then(
+		(data) => {
+			AssertCodeCallback(data, 200, context);
+			resolve();
+		});
 }
 
 // Label Testing
-function testLabels() {
+function testLabels(resolve, reject) {
 	const context = "[testLabels]";
-	manager.GetLabels('johnmlee101', 'GitHub-Project-Manager', 178260045, AssertCodeCallback(304, context), errorCallback, false);
+	manager.GetLabels(repoOwner, repo, 178260045, false)
+	.then(
+		(data) => {
+			AssertCodeCallback(data, 200, context);
+			AssertCountCallback(data, 7, context);
+			resolve();
+		});
 }
 
-setTimeout(testCache, 0);
-setTimeout(testCards, 5000);
-setTimeout(testIssues, 7000);
-setTimeout(testLabels, 9000);
+var tests = [];
+tests.push(testCards);
+tests.push(testColumns);
+tests.push(testProjects);
+tests.push(testIssues);
+tests.push(testIssue);
+tests.push(testLabels);
+tests.push(testCache);
 
+function testNext() {
+	var next = tests.pop();
+	if (_.isUndefined(next)) {
+		return;
+	}
+	new Promise(next).then(() => { testNext() });
+}
+
+testNext();
+
+//testNext();
 
